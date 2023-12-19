e29ade78eea27c740c116b350f5d31f7945d97ba
choyoon88 <>
2023-12-19 10:02:16 +0000

    page project hypothesis

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import project_summary
 from app_pages.page_price_cor_study import page_sale_price_study_body
+from app_pages.page_project_hypothesis import page_project_hypothesis
+
 
 app = MultiPage(app_name="Housing Price Predictor")
+
 
 # add app pages
 app.add_page("Quick Project Summary", project_summary)
 app.add_page("House Price Correlation Study", page_sale_price_study_body)
+app.add_page("Project Hypothesis", page_project_hypothesis)
+
 
 # run the app
 app.run()
---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+
+def page_project_hypothesis():
+    """
+    Display project hypothesis after correlation study
+    """
+
+    st.write("### Project Hypothesis")
+
+    st.success(
+        f"The conclusion we could make from our observations are: \n\n"
+        f"* Size matters: 1stFlrSF, GarageArea, GrLivArea, TotalBsmtSF shows "
+        f"that generally when the size is bigger, the sale price grows higher."
+        f"\n"
+        f"  * We can make our hypothesis 1 as **The size of a house positively "
+        f"correlates with the housing price**\n"
+        f"* Remodeling year matters: YearRemodAdd shows when there was a "
+        f"recent remodel with the house, the sale price is higher."
+        f"\n"
+        f"  * We can make our hypothesis 2 as **Year of the house remodeling "
+        f"positively correlates with the housing price**\n"
+        f"* Quality matters: KitchenQual and OverallQual shows that houses "
+        f"with higher quality have a higher sale price.\n"
+        f"  * We can make our hypothesis 3 as **The quality of the house "
+        f"positively correlates with the housing price**"
+    )
