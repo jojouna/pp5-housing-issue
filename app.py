c10f4a0bf6d346903bfca2602cc741b87155dbb0
choyoon88 <>
2023-12-19 21:41:08 +0000

    start ml_predict_price page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.page_summary import project_summary
 from app_pages.page_price_cor_study import page_sale_price_study_body
 from app_pages.page_project_hypothesis import page_project_hypothesis
 from app_pages.page_sale_price_predictor import page_sale_price_predictor
+from app_pages.page_ml_predict_price import page_ml_predict_price
 
 
 app = MultiPage(app_name="Housing Price Predictor")
@@ -16,6 +17,7 @@ app.add_page("Quick Project Summary", project_summary)
 app.add_page("House Price Correlation Study", page_sale_price_study_body)
 app.add_page("Project Hypothesis", page_project_hypothesis)
 app.add_page("Sale Price Predictor", page_sale_price_predictor)
+app.add_page("ML: House Price Prediction", page_ml_predict_price)
 
 
 # run the app
---
+++ app_pages/page_ml_predict_price.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+def page_ml_predict_price():
+    st.write(
+        f"### ML: Predict Sale Price"
+    )
